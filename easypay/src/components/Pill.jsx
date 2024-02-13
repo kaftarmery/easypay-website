a135c6516ecf9142890be4847c1af5e97fd7bfda
meryemkaftar <merykft@gmail.com>
2024-02-13 19:56:39 +0300

    Added some animation to the "pill" buttons in the hero sec.

--- easypay/src/components/Pill.jsx
+++ easypay/src/components/Pill.jsx
@@ -1,4 +1,6 @@
 import React from "react";
+import { useRef } from "react";
+import { motion } from "framer-motion";
 
 // TODO:
 // - make font lighter (add to custom fonts)
@@ -6,20 +8,54 @@ import React from "react";
 // add some animations (subtle)
 
 export const Pill = () => {
-  return (
-    <div>
-      {/* left side pills */}
-      <div className="absolute left-2 top-20 transform rotate-12  bg-pill-gray border-[1px] border-pill-border text-gray-400 font-thiccboi-regular py-3 px-5 rounded-full w-[200px]">
-        Solutions
-      </div>
+  const constraintsRef = useRef(null);
 
-      <div className="absolute left-[5rem] bottom-[-1rem] transform rotate-[-15deg] bg-pill-gray border-[1px] border-pill-border text-gray-400 font-thiccboi-regular py-3 px-5 rounded-full w-[200px]">
+  return (
+    <motion.div ref={constraintsRef} className="cursor-pointer">
+      {/* left side pills container */}
+
+      {/* first set of pills */}
+      <motion.div
+        drag
+        dragConstraints={constraintsRef}
+        dragElastic={0.4}
+        className="absolute left-2 top-20 transform rotate-12"
+      >
+        <div className="bg-pill-gray transform rotate-12 border-[1px] border-pill-border text-gray-400 font-thiccboi-regular py-3 px-5 rounded-full w-[200px]">
+          Solutions
+        </div>
+      </motion.div>
+      <motion.div
+        drag
+        dragConstraints={constraintsRef}
+        dragElastic={0.4}
+        className="absolute left-2 top-20 transform rotate-12"
+      >
+        <div className="bg-pill-gray hover:bg-red-200 transform rotate-12 border-[1px] border-pill-border text-gray-400 font-thiccboi-regular py-3 px-5 rounded-full w-[200px]">
+          Better
+        </div>
+      </motion.div>
+
+      {/* second set of pills */}
+      <motion.div
+        drag
+        dragConstraints={constraintsRef}
+        className="absolute left-[5rem] bottom-[-1rem] transform rotate-[-15deg] bg-pill-gray border-[1px] border-pill-border text-gray-400 font-thiccboi-regular py-3 px-5 rounded-full w-[200px]"
+      >
+        Payments
+      </motion.div>
+
+      <motion.div
+        drag
+        dragConstraints={constraintsRef}
+        className="absolute left-[5rem] bottom-[-1rem] transform rotate-[-15deg] bg-pill-gray border-[1px] border-pill-border text-gray-400 font-thiccboi-regular py-3 px-5 rounded-full w-[200px]"
+      >
         Simplify
-      </div>
+      </motion.div>
 
       <div className="absolute left-[-5rem] bottom-20 transform rotate-[-15deg] bg-pill-gray border-[1px] border-pill-border text-gray-400 font-thiccboi-regular py-3 px-5 rounded-full w-[200px]">
         Manage
       </div>
-    </div>
+    </motion.div>
   );
 };
